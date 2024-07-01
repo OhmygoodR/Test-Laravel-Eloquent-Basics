b3a97fe492e77566385fd7374ec59be62b46f6d2
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2024-07-02 02:01:15 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Models\Project;
-use App\Observers\ProjectObserve;
+use App\Observers\ProjectObserver;
 
 class AppServiceProvider extends ServiceProvider
 {
